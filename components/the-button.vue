2638872a2463d75700fa25272a456ba9dea3bcd4
Vladislav Efanov <vladislav.efanov@gmail.com>
2023-07-30 15:19:24 +0200

    feat: add TheButton cmp

---
+++ components/the-button.vue
@@ -0,0 +1,32 @@
+<template>
+    <button class="btn btn-primary" type="button" :disabled="isLoading">
+      <span v-if="isLoading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+      {{ buttonText }}
+    </button>
+</template>
+
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+
+export default defineComponent({
+    props: {
+        text: {
+            type: String,
+            required: true,
+        },
+        isLoading: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    setup(props) {
+        const buttonText = computed(() => (props.isLoading ? 'Loading...' : props.text));
+
+        return {
+            buttonText,
+        };
+    },
+});
+</script>
+
