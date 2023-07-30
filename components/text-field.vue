d7ea6cd2859c2f02a1a70e7a4b5563a04c755994
Vladislav Efanov <vladislav.efanov@gmail.com>
2023-07-30 15:19:31 +0200

    feat: add TextField cmp

---
+++ components/text-field.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="form-floating mb-3">
+        <input
+            :value="modelValue"
+            :type="typeInput"
+            :placeholder="placeholder"
+            class="form-control"
+            :id="`${label}-floatingInputValue`"
+            required
+            @input="$emit('update:modelValue', $event.target.value)"
+        >
+        <label for="floatingInputGrid">{{ label }}</label>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        typeInput: {
+            type: String,
+            default: 'text',
+        },
+        modelValue: {
+            type: String,
+            default: '',
+        },
+        placeholder: {
+            type: String,
+            default: '',
+        },
+        label: {
+            type: String,
+            default: '',
+        },
+    },
+    emits: ['update:modelValue'],
+});
+</script>
+
