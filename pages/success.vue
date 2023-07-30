a3890f6524873859d745fb75d4130bb882773d33
Vladislav Efanov <vladislav.efanov@gmail.com>
2023-07-30 18:23:05 +0200

    feat: add link of github

--- pages/success.vue
+++ pages/success.vue
@@ -1,5 +1,14 @@
 <template>
     <div class="alert alert-success" role="alert">
-        Вы успешно вошли в систему!
+        <p>Вы успешно вошли в систему!</p>
+
+        <div class="d-flex align-items-center">
+            <img src="../public/icon-github.svg" alt="github icon" width="30" heihgt="30">
+            <a
+                class="btn-link ms-2"
+                target="_blank"
+                href="https://github.com/vladdof/nuxt-auth-page"
+            >Ссылка на репозиторий</a>
+        </div>
     </div>
 </template>
