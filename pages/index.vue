314df145f5f6bf8196d526481631a134637376b6
Vladislav Efanov <vladislav.efanov@gmail.com>
2023-07-30 15:19:51 +0200

    feat: add index page

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="alert alert-info" role="alert">
+        <p class="mb-4">
+            Для того, чтобы попасть в систему Вам необходимо войти
+        </p>
+
+        <NuxtLink
+            class="btn btn-primary"
+            to="/log-in"
+        >
+            Вход
+        </NuxtLink>
+    </div>
+</template>
